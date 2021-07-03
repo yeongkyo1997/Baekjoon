eb2cc1aa2fcde9f31ccca6c6bade2b29ab1ed4bf
Yeong <yeongkyo1997@gmail.com>
2021-07-03 15:15:16 +0900

    main

---
+++ Input and Output and Private Operations/1008.cpp
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/1008
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+	double a, b;
+	cin >> a >> b;
+	printf("%.9lf", a / b);
+}
---
+++ Input and Output and Private Operations/10998.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/10998
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+}
