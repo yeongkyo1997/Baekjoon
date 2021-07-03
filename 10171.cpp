fc36c81c0cc31d69a579c72188618072e4f9622f
yeongkyo1997 <54164709+yeongkyo1997@users.noreply.github.com>
2021-07-03 14:41:01 +0900

    Add files via upload

---
+++ 10171.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/10171
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+}
