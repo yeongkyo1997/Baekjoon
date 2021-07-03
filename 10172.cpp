e88f304ff58adf1a78d18cca46b0fc5601dae8e9
yeongkyo1997 <54164709+yeongkyo1997@users.noreply.github.com>
2021-07-03 15:03:49 +0900

    Add files via upload

---
+++ 10172.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10172
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+}
