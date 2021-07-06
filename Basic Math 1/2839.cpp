c691fcaf4c0207e3e033162e4afca3c8904e04f1
Yeong <yeongkyo1997@gmail.com>
2021-07-06 15:28:22 +0900

    main

---
+++ Basic Math 1/2839.cpp
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/2839
+
+#include<stdio.h>
+ 
+int main() {
+    int s, B = 0;
+ 
+    scanf("%d", &s);
+ 
+    while (1) {
+        if (s % 5 == 0) {
+            printf("%d", s/5 + B);
+            break;
+        }
+        else if (s <= 0) {
+            printf("-1");
+            break;
+        }
+        s -= 3;
+        B++;
+    }
+ 
+    return 0;
+}
