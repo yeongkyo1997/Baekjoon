e9abc911e99b84efec8e10943f3aec6050245f13
Yeong <yeongkyo1997@gmail.com>
2021-07-07 10:50:00 +0900

    main

---
+++ Basic Math 2/11653.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/11653
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			cout << i << endl;
+			n /= i;
+		}
+	}
+
+	if (n != 1) 
+		cout << n;
+}
