d1b50429d13a96afc2c8d2dd13c8d82894dfebbe
Yeong <yeongkyo1997@gmail.com>
2021-07-08 19:43:21 +0900

    main

---
+++ 2445.cpp
@@ -0,0 +1,42 @@
+// https://www.acmicpc.net/problem/2445
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        for (int j = 1; j <= n * 2 - i * 2; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        for (int j = 1; j <= n * 2 - i * 2; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
