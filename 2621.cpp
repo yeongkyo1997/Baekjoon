83b74cdaf7b0490fef3ce6367f0ad9311050983c
Yeong <yeongkyo1997@gmail.com>
2021-07-10 15:58:02 +0900

    main

--- 2621.cpp
+++ 2621.cpp
@@ -1,6 +1,7 @@
 // https://www.acmicpc.net/problem/2621
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,35 +24,171 @@ bool compare(const Card c1, const Card c2) {
 
 int main() {
     Card c[5];
-    
+    int max = 0;
+    bool check = true;
     for (int i = 0; i < 5; i++) {
         cin >> c[i].color >> c[i].num;
     }
 
     sort(c, c + 5, compare);
 
-    if (c[0].color == c[1].color && c[1].color == c[2].color && c[2].color == c[3].color && c[4].color == c[5].color) {
-        if (c[0].num + 4 == c[1].num + 3 && c[1].num + 3 == c[2].num + 2 && c[3].num + 2 == c[4].num + 1 && c[4].num + 1 == c[5].num) {
-            cout << 900 + c[5].num;
-            return;
+    // Rule 1
+    
+    for (int i = 0; i < 4; i++) {
+        if (c[i].num + 1!= c[i + 1].num || c[i].color != c[i + 1].color) {
+            check = false;
+            break;
         }
     }
-    
-    for (int i = 0; i < 5; i++) {
-        int cnt = 0;
-        int start;
-        for (int j = i; j < 4; j++) {
-            if (c[j].num == c[j + 1].num) {
-                start = c[j].num;
-                cnt++;
+    if (check) {
+        cout << 900 + c[4].num;
+        return 0;
+    }
+
+    // Rule 2
+
+    for (int i = 0; i < 2; i++) {
+        check = true;
+        for (int j = i; j < i + 3; j++) {
+            if (c[j].num != c[j + 1].num){
+                check = false;
+            }
+        }
+        if (check) {
+            cout << 800 + c[i].num;
+            return 0;
+        }   
+    }
+
+    // Rule 3
+
+    int three;
+    int two;
+
+    // 3개가 같은 경우
+    check = true;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = i; j < i + 2; j++) {
+            if (c[j].num != c[j + 1].num) {
+                check = false;
             }
         }
 
-        if (cnt >= 4) {
-            cout << 800 + start;
-            return;
+        if (check) {
+            three = c[i].num;
+            break;
+        }
+    }
+    
+    // 2개가 같은 경우
+    if (check) {
+        check = true;
+
+        for (int i = 0; i < 4; i++) {
+            if (c[i].num != c[i + 1].num || c[i].num == three) {
+                check = false;
+            }
+            else {
+                check = true;
+            }
+
+            if (check) {
+                two = c[i].num;
+                cout << three * 10 + two + 700;
+                return 0;
+            }
         }
     }
 
+    // Rule 4
+    check = true;
+
+    for (int i = 0; i < 4; i++) {
+        if (c[i].color != c[i + 1].color) {
+            check = false;
+        }
+    }
+
+    if (check) {
+        cout << 600 + c[4].num;
+        return 0;
+    }
+
+    // Rule 5
+
+    check = true;
+        
+    for (int j = 0; j < 4; j++) {
+        if (c[j].num + 1  != c[j + 1].num) {
+            check = false;
+        }
+    }
+    if (check) {
+        cout << 500 + c[4].num;
+        return 0;
+    }
+
+    // Rule 6
+
+    for(int i = 0; i < 3; i ++) {
+        check = true;
+        for (int j = i; j < i + 2; j++) {
+            if (c[j].num != c[j + 1].num) {
+                check = false;
+            }
+        }
+        if (check) {
+            cout << 400 + c[i].num;
+            return 0;
+        }
+    }
+
+    // Rule 7
+    int result = 300;
+    int bigStart;
     
+    for(int i = 0; i < 4; i ++) {
+        if (c[i].num != c[i + 1].num) {
+            check = false;
+        }
+        else {
+            check = true;
+        }
+        if (check) {
+            result += c[i].num;
+            bigStart = i + 1;
+            break;
+        }
+    }
+    
+    if (check) {
+        for(int i = bigStart; i < 4; i ++) {
+
+            if (c[i].num != c[i + 1].num) {
+                check = false;
+            }
+            else {
+                check = true;
+            }
+            if (check) {
+                result += c[i].num * 10;
+                cout << result;
+                return 0;
+            }
+        }
+    }
+
+    // Rule 8
+
+    for (int i = 0; i < 4; i ++) {
+        if (c[i].num == c[i + 1].num) {
+            cout << 200 + c[i].num;
+            return 0;
+        }
+    }
+
+    // Rule 9
+
+    cout << 100 + c[4].num;
 }
