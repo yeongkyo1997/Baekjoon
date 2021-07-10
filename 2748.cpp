eb7dbf2edf0fc34fd7ce9ad47b3657291c784288
Yeong <yeongkyo1997@gmail.com>
2021-07-10 14:17:04 +0900

    main

---
+++ 2748.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/2747
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    long long int arr[91];
+
+    cin >> n;
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    cout << arr[n];
+}
