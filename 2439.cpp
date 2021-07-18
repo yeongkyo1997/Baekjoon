87e4674aedd7ee4aabb28b070b6d7fad9145e24b
Yeong <yeongkyo1997@gmail.com>
2021-07-18 14:13:52 +0900

    main

---
+++ 2439.cpp
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/2439
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++)
+            cout << " ";
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
