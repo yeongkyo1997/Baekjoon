7b7d46c3e6025b04f8b641fdc95a6ffae9ca73ee
Yeong <yeongkyo1997@gmail.com>
2021-07-26 21:06:32 +0900

    main

---
+++ 2743.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/2743
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    cout << str.length();
+}
