fd8ec36448ccb8aeb5d82dfa09c559c455ef856e
Yeong <yeongkyo1997@gmail.com>
2021-07-26 21:04:14 +0900

    main

---
+++ 9086.cpp
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/9086
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) { 
+        string str;
+
+        cin >> str;
+        cout << str[0] << str[str.length() - 1] << endl;
+    }
+}
