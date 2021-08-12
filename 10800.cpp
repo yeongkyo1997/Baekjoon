fc65d31d9cdc09d79ddc5da8af5bda19ec594eec
Yeong <yeongkyo1997@gmail.com>
2021-08-12 17:27:05 +0900

    main

---
+++ 10800.cpp
@@ -0,0 +1,41 @@
+// https://www.acmicpc.net/problem/10800
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+struct Ball {
+    int color;
+    int size;
+};
+
+bool compare(const Ball a, const Ball b) {
+    if (a.size < b.size)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    Ball b[200000];
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d %d", &b[i].color, &b[i].size);
+
+    sort(b, b + n, compare);
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i + 1; j < n; j++) {
+            if (b[i].color != b[j].color)
+                sum += b[j].size;
+            else
+                continue;
+        }
+        printf("%d\n", sum);
+    }
+}
